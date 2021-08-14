38288458fab8f1fe8a7337d6a490377ec198176a
Luke Briggs <lukebriggs02@gmail.com>
2021-08-14 12:02:34 +0100

    Add redirect for twitter explore page

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-var host = "*://*.youtube.com";
+// YouTube
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         return {redirectUrl: "https://youtube.com/feed/subscriptions"};
@@ -6,6 +6,20 @@ chrome.webRequest.onBeforeRequest.addListener(
     {
         urls: [
             "*://*.youtube.com/"
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
+
+// Twitter
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        return {redirectUrl: "https://twitter.com/"};
+    },
+    {
+        urls: [
+            "*://twitter.com/explore"
         ],
         types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
     },
