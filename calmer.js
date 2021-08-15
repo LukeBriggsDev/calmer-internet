3627afc2d4386e6ccaa8497dafeb42441c066b00
Luke Briggs <lukebriggs02@gmail.com>
2021-08-15 13:37:47 +0100

    Implement options with extension

--- calmer.js
+++ calmer.js
@@ -1,30 +1,72 @@
 console.log("LOADED CALMER INTERNET ON " + window.location.hostname);
+// Settings
+settings = {}
 
+let getting = chrome.storage.sync.get([
+    "YTHomeLinks",
+    "YTHomeRedirect",
+    "YTExploreLinks",
+    "YTComments",
+    "YTWatchNext",
+    "YTLiveChat",
+
+    "TwitterTrendingBar",
+    "TwitterExploreLinks",
+    "TwitterExploreRedirect",
+    "TwitterWhoToFollow",
+    "TwitterTopics",
+
+    "InstaComments",
+    "InstaExploreLinks",
+    "InstaFeedComments"
+], function (result){
+    settings = {
+        "YTHomeLinks": result.YTHomeLinks,
+        "YTHomeRedirect": result.YTHomeRedirect,
+        "YTExploreLinks": result.YTExploreLinks,
+        "YTComments": result.YTComments,
+        "YTWatchNext": result.YTWatchNext,
+        "YTLiveChat": result.YTLiveChat,
+
+        "TwitterTrendingBar": result.TwitterTrendingBar,
+        "TwitterExploreLinks": result.TwitterExploreLinks,
+        "TwitterExploreRedirect": result.TwitterExploreRedirect,
+        "TwitterWhoToFollow": result.TwitterWhoToFollow,
+        "TwitterTopics": result.TwitterTopics,
+
+        "InstaComments": result.InstaComments,
+        "InstaExploreLinks": result.InstaExploreLinks,
+        "InstaFeedComments": result.InstaFeedComments
+    }
+})
 function getElementsToRemove(){
-    var elementsToRemove; 
+    var elementsToRemove = {};
     // YouTube
-    if (window.location.hostname == "www.youtube.com" || window.location.hostname == "m.youtube.com"){
-        // Set mobile flags
-        if(window.location.href == "https://m.youtube.com/"){
-            var IS_MOBILE = true;
+    if (window.location.hostname === "www.youtube.com" || window.location.hostname === "m.youtube.com"){
+
+        if(settings["YTHomeLinks"]){
+            elementsToRemove["homeLinks"] = document.querySelectorAll("a[href='/']")
         }
-        else{
-            var IS_MOBILE = false
+        if(settings["YTexploreLinks"]){
+            elementsToRemove["exploreLinks"] = document.querySelectorAll("a[href='/feed/explore']")
         }
-            
-        elementsToRemove = {
-            "homeLinks": document.querySelectorAll("a[href='/']"),
-            "exploreLinks": document.querySelectorAll("a[href='/feed/explore']"),
-            "comments": document.getElementsByClassName("ytd-comments"),
-            "commentsMobile": document.getElementsByTagName("ytm-comments-entry-point-header-renderer"),
-            "watchNext": document.getElementsByClassName("ytd-watch-next-secondary-results-renderer"),
-            "watchNextMobile": document.querySelectorAll("ytm-item-section-renderer[section-identifier='related-items']"),
-            "liveChat": document.getElementsByTagName("ytd-live-chat-frame"),
+        if(settings["YTComments"]){
+            elementsToRemove["comments"] = document.getElementsByClassName("ytd-comments")
+            elementsToRemove["commentsMobile"] = document.getElementsByTagName("ytm-comments-entry-point-header-renderer")
+        }
+        if(settings["YTWatchNext"]){
+            elementsToRemove["watchNext"] = document.getElementsByClassName("ytd-watch-next-secondary-results-renderer")
+            elementsToRemove["watchNextMobile"] = document.querySelectorAll("ytm-item-section-renderer[section-identifier='related-items']")
+        }
+        if(settings["YTLiveChat"]){
+            elementsToRemove["liveChat"] = document.getElementsByTagName("ytd-live-chat-frame")
         }
         // Elements that might throw undefined errors
         try{
-            elementsToRemove["homeMobile"] = [document.getElementsByClassName("pivot-bar-item-tab pivot-w2w")[0].parentElement]
-            elementsToRemove["trendingMobile"] = [document.getElementsByClassName("pivot-bar-item-tab pivot-trending")[0].parentElement]
+            if(settings["YTHomeLinks"]) {
+                elementsToRemove["homeMobile"] = [document.getElementsByClassName("pivot-bar-item-tab pivot-w2w")[0].parentElement]
+                elementsToRemove["trendingMobile"] = [document.getElementsByClassName("pivot-bar-item-tab pivot-trending")[0].parentElement]
+            }
         }
         catch(e){
 
@@ -32,47 +74,57 @@ function getElementsToRemove(){
     }
 
     //Twitter
-    if (window.location.hostname == "twitter.com" || window.location.hostname == "mobile.twitter.com"){
-        elementsToRemove = {
-            "trendingBar": document.querySelectorAll("div[aria-label='Timeline: Trending now']"),
-            "exploreLinks": document.querySelectorAll("a[href='/explore']"),
-            "whoToFollow": document.querySelectorAll("aside[aria-label='Who to follow']"),
-            "topics": document.querySelectorAll("div[aria-label='Timeline: ']"),
-            "miscStyling": document.getElementsByClassName("css-1dbjc4n r-1867qdf r-1phboty r-rs99b7 r-1ifxtd0 r-1bro5k0 r-1udh08x")
+    if (window.location.hostname === "twitter.com" || window.location.hostname === "mobile.twitter.com"){
+        if(settings["TwitterTrendingBar"]){
+            elementsToRemove["trendingBar"] = document.querySelectorAll("div[aria-label='Timeline: Trending now']")
+        }
+        if(settings["TwitterExploreLinks"]){
+            elementsToRemove["exploreLinks"] = document.querySelectorAll("a[href='/explore']")
+        }
+        if(settings["TwitterWhoToFollow"]){
+            elementsToRemove["miscStyling"] = document.getElementsByClassName("css-1dbjc4n r-1867qdf r-1phboty r-rs99b7 r-1ifxtd0 r-1bro5k0 r-1udh08x")
+            elementsToRemove["whoToFollow"] = document.querySelectorAll("aside[aria-label='Who to follow']")
+        }
+        if(settings["TwitterTopics"]){
+            elementsToRemove["topics"] = document.querySelectorAll("div[aria-label='Timeline: ']")
         }
     }
 
     //Instagram
-    if (window.location.hostname == "www.instagram.com"){
-        elementsToRemove = {
-            "comments": document.getElementsByClassName("Mr508 ")
+    if (window.location.hostname === "www.instagram.com"){
+        if(settings["InstaComments"]){
+            elementsToRemove["comments"] = document.getElementsByClassName("Mr508 ")
         }
+
         // Elements that might throw undefined errors
 
         //Explore Links
-        try{
-            exploreLinkParents = []
-            for(element of document.querySelectorAll("a[href='/explore/']")){
-                exploreLinkParents.push(element.parentElement)
+        if(settings["InstaExploreLinks"]) {
+
+            try {
+                exploreLinkParents = []
+                for (element of document.querySelectorAll("a[href='/explore/']")) {
+                    exploreLinkParents.push(element.parentElement)
+                }
+                for (element of document.querySelectorAll("a[href='/explore/people/']")) {
+                    exploreLinkParents.push(element.parentElement.parentElement)
+                }
+                elementsToRemove["exploreLinkParents"] = exploreLinkParents
+            } catch (e) {
+                console.log(e)
             }
-            for(element of document.querySelectorAll("a[href='/explore/people/']")){
-                exploreLinkParents.push(element.parentElement.parentElement)
-            } 
-            elementsToRemove["exploreLinkParents"] = exploreLinkParents
-        }
-        catch(e){
-            console.log(e)
         }
         // Comments below posts in feed
-        try{   
-            feedComments = []
-            for(element of document.getElementsByClassName("r8ZrO")){
-                feedComments.push(element.parentElement.parentElement);
+        if(settings["InstaFeedComments"]) {
+            try {
+                feedComments = []
+                for (element of document.getElementsByClassName("r8ZrO")) {
+                    feedComments.push(element.parentElement.parentElement);
+                }
+                elementsToRemove["feedComments"] = feedComments;
+            } catch (e) {
+
             }
-            elementsToRemove["feedComments"] = feedComments;
-        }
-        catch(e){
-            
         }
     }
     
